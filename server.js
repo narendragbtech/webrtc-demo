c2481b2689d481fb989d823974e0ede6150dbe20
root <root@ip-172-31-45-215.ap-south-1.compute.internal>
2020-10-26 10:16:27 +0000

    server update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/meeting", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 //Listen on port 3000
 server = app.listen(port, () => {
   console.log(`Server Running on port ${port}`);
