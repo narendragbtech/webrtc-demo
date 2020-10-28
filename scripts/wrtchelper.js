8131654b0454c10cfeae7a819476cd897538ac5a
SoftwareClan <53153286+SoftwareClan@users.noreply.github.com>
2020-10-28 11:45:57 +0530

    comment more stun server

--- scripts/wrtchelper.js
+++ scripts/wrtchelper.js
@@ -7,11 +7,11 @@ var WrtcHelper = (function () {
         credential: "admin@123",
       },
       { urls: "stun:stun.docango.com:3478" },
-      { urls: "stun:stun.l.google.com:19302" },
-      { urls: "stun:stun1.l.google.com:19302" },
-      { urls: "stun:stun2.l.google.com:19302" },
-      { urls: "stun:stun3.l.google.com:19302" },
-      { urls: "stun:stun4.l.google.com:19302" },
+      // { urls: "stun:stun.l.google.com:19302" },
+      // { urls: "stun:stun1.l.google.com:19302" },
+      // { urls: "stun:stun2.l.google.com:19302" },
+      // { urls: "stun:stun3.l.google.com:19302" },
+      // { urls: "stun:stun4.l.google.com:19302" },
     ],
   };
 
